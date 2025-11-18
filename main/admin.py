761fb5b0c2f852c68d00153546f0da5f0d233720
andrebeltrao <andrebeltrao78@gmail.com>
2025-11-18 11:55:42 -0300

    criação html
    criou-se o primeiro html

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from main.models import Task
 
-# Register your models here.
+
+#admin.site.register(Task)~
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'usuario',)
+    list_filter = ('concluida', 'prioridade',)
+    search_fields = ('titulo',)
